adb542880014732dd230a9b6502706d70b753335
Jc Din <ozknightwalker62@gmail.com>
2016-08-25 20:39:05 +0800

    replace redirect to httpresponse

--- core/magic/views.py
+++ core/magic/views.py
@@ -12,25 +12,30 @@ import re
 
 # Create your views here.
 
+
 class HomeView(TemplateView):
-	template_name = 'home.html'
+    template_name = 'home.html'
 
 
 class MessageFacebookUser(View):
-	def post(self, request, *args, **kwargs):
-		base_cmd = 'python message_fb.py'
-		image = request.POST.get('image', None)
-		username = ''
-		index = 0
+    def post(self, request, *args, **kwargs):
+        base_cmd = 'python message_fb.py'
+        image = request.POST.get('image', None)
+        username = ''
+        index = 0
 
-		filename = str(image).split('.')[0]
-		if filename.isdigit():
-			if int(filename) < 6:
-				index = int(filename)
+        filename = str(image).split('.')[0]
+        if filename.isdigit():
+            if int(filename) < 6:
+                index = int(filename)
 
-		username = usernames.usernames[index]
+        username = usernames.usernames[index]
 
-		if image is not None:
-			os.system('{} {}'.format(base_cmd, username))
+        if image is not None:
+            try:
+                os.system('{} {}'.format(base_cmd, username))
+            except:
+                return HttpResponse(status=400)
 
-		return redirect(reverse('home'))
+        # return redirect(reverse('home'))
+        return HttpResponse()
