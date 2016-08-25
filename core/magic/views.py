3a856c6a629d49fff5e38dab57b20e2488689f85
justin <justin.mearns@gmail.com>
2016-08-25 19:51:11 +0800

    aadklasjdlask

--- core/magic/views.py
+++ core/magic/views.py
@@ -1,7 +1,32 @@
+import os
+
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, View
+from django.http import HttpResponse
+
+import usernames
 
 # Create your views here.
 
 class HomeView(TemplateView):
 	template_name = 'home.html'
+
+
+class MessageFacebookUser(View):
+	def post(self, request, *args, **kwargs):
+		base_cmd = 'python message_fb.py'
+		image = request.POST.get('image', None)
+		username = ''
+		index = 0
+
+		filename = str(image).split('.')[0]
+		if filename.isdigit():
+			if int(filename) < 6:
+				index = int(filename)
+
+		username = usernames.usernames[index]
+
+		if image is not None:
+			os.system('{} {}'.format(base_cmd, username))
+		return reverse('home')
